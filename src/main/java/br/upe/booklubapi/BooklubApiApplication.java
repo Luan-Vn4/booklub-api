e09811669832bb6f934723660bf98d6be80d0639
Luan-Vn4 <luanvilaca1@gmail.com>
2025-03-17 22:21:34 -0300

    first commit

---
+++ src/main/java/br/upe/booklubapi/BooklubApiApplication.java
@@ -0,0 +1,13 @@
+package br.upe.booklubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/upe/booklubapi/BooklubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.booklubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
