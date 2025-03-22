fd8c75995ee042fa8ab344f4994e882d32ba14b0
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-03-22 10:59:40 -0300

    fix: changing user id generation strategy to UUID

--- src/main/java/br/upe/booklubapi/domain/entities/User.java
+++ src/main/java/br/upe/booklubapi/domain/entities/User.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
     private Integer id;
     
