a5f6bdf277a96a51ac156bcecabbd1b540a63db3
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-03-22 11:21:09 -0300

    feat: building delete user service and controller

---
+++ src/main/java/br/upe/booklubapi/app/services/UserServices/DeleteUserService.java
@@ -0,0 +1,32 @@
+package br.upe.booklubapi.app.services.UserServices;
+
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.upe.booklubapi.app.services.Command;
+import br.upe.booklubapi.domain.entities.User;
+import br.upe.booklubapi.domain.repositories.UserRepository;
+
+@Service
+public class DeleteUserService implements Command<Integer, Void>{
+
+    private UserRepository userRepository;
+
+    public DeleteUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    @Override
+    public ResponseEntity<Void> execute(Integer id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        
+        if(optionalUser.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return null;
+    }
+}
--- src/main/java/br/upe/booklubapi/presentation/controllers/UserController.java
+++ src/main/java/br/upe/booklubapi/presentation/controllers/UserController.java
@@ -1,24 +1,34 @@
 package br.upe.booklubapi.presentation.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.upe.booklubapi.app.DTOs.user.UserDTO;
 import br.upe.booklubapi.app.services.UserServices.CreateUserService;
+import br.upe.booklubapi.app.services.UserServices.DeleteUserService;
 import br.upe.booklubapi.domain.entities.User;
 
 @RestController
 public class UserController {
     private final CreateUserService createUserService;
+    private final DeleteUserService deleteUserService;
 
-    public UserController(CreateUserService createUserService) {
+    public UserController(CreateUserService createUserService, DeleteUserService deleteUserService) {
         this.createUserService = createUserService;
+        this.deleteUserService = deleteUserService;
     }
 
     @PostMapping("/user")
     public ResponseEntity<UserDTO> createUser(@RequestBody User user) {
         return createUserService.execute(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        return deleteUserService.execute(id);
     }
 }
