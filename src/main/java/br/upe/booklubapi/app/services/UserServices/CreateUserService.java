9c6b078acdd730163c4ec6a4f025e589855f566a
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-03-22 10:51:03 -0300

    feat: building create user service

--- src/main/java/br/upe/booklubapi/app/services/UserServices/CreateUserService.java
+++ src/main/java/br/upe/booklubapi/app/services/UserServices/CreateUserService.java
@@ -1,8 +1,27 @@
 package br.upe.booklubapi.app.services.UserServices;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.upe.booklubapi.app.services.Command;
+import br.upe.booklubapi.domain.entities.User;
+import br.upe.booklubapi.domain.repositories.UserRepository;
+
 @Service
-public class CreateUserService {
+public class CreateUserService implements Command<User, UserDTO>{
+
+    private UserRepository userRepository;
+
+    public CreateUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public ResponseEntity<UserDTO> execute(User user) {
+        
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UserDTO(savedUser));
+    }
     
 }
