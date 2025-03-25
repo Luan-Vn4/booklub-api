02a3ba05f0ba42a5f52d480edf758d2a6fd6bd78
Luan-Vn4 <luanvilaca1@gmail.com>
2025-03-25 13:49:13 -0300

    fix: corrigido ReadableRepository para usar generics

--- src/main/java/br/upe/booklubapi/domain/core/repositories/ReadableRepository.java
+++ src/main/java/br/upe/booklubapi/domain/core/repositories/ReadableRepository.java
@@ -1,10 +1,9 @@
 package br.upe.booklubapi.domain.core.repositories;
 
-import java.util.Collection;
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.util.Collection;
+import java.util.Optional;
 
 public interface ReadableRepository<T, ID> {
 
@@ -14,7 +13,7 @@ public interface ReadableRepository<T, ID> {
 
     Page<T> findAll(Pageable pageable);
 
-    Page<T> findAllByIdIn(Collection<ID> id, Pageable pageable);
+    Page<T> findAllByIdIn(Collection<ID> uuids, Pageable pageable);
 
     long count();
 
