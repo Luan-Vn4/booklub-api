89840b8a98ffd95f05268f295eb7c55fe57f7475
Luan-Vn4 <luanvilaca1@gmail.com>
2025-03-25 14:26:38 -0300

    docs: renomeados grupos de endpoints no OpenAPI doc

--- src/main/java/br/upe/booklubapi/presentation/controllers/clubs/ClubController.java
+++ src/main/java/br/upe/booklubapi/presentation/controllers/clubs/ClubController.java
@@ -3,12 +3,17 @@ package br.upe.booklubapi.presentation.controllers.clubs;
 import br.upe.booklubapi.app.clubs.dtos.ClubDTO;
 import br.upe.booklubapi.app.clubs.dtos.CreateClubDTO;
 import br.upe.booklubapi.app.clubs.dtos.UpdateClubDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Tag(
+    name="Book Clubs",
+    description="Endpoints to get book clubs"
+)
 public interface ClubController {
 
     ResponseEntity<ClubDTO> create(CreateClubDTO dto);
