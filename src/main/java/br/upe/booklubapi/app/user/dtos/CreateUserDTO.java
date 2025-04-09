f0a7d0ed6d6cf214c390429085ff8c1649b8a26d
Luan-Vn4 <luanvilaca1@gmail.com>
2025-04-09 14:59:36 -0300

    fix: mudado CreateUserDTO para receber url da imagem temporariamente

--- src/main/java/br/upe/booklubapi/app/user/dtos/CreateUserDTO.java
+++ src/main/java/br/upe/booklubapi/app/user/dtos/CreateUserDTO.java
@@ -1,12 +1,10 @@
 package br.upe.booklubapi.app.user.dtos;
 
-import org.springframework.web.multipart.MultipartFile;
-
 public record CreateUserDTO(
     String username,
     String email,
     String firstName,
     String lastName,
-    MultipartFile image,
+    String imageUrl,
     String password
 ) {}
