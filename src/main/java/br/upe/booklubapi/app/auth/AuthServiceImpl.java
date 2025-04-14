fc85a3c01a21f4edb43884a5475a7bc7b1256d43
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-04-14 14:58:25 -0300

    refactoring: refatorando o codigo de criaçao criaçao de user pra incluir logica de adicionar imagem de perfil no minIO

--- src/main/java/br/upe/booklubapi/app/auth/AuthServiceImpl.java
+++ src/main/java/br/upe/booklubapi/app/auth/AuthServiceImpl.java
@@ -1,10 +1,16 @@
 package br.upe.booklubapi.app.auth;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import br.upe.booklubapi.app.auth.dto.AuthBody;
 import br.upe.booklubapi.app.auth.dto.KeycloakTokenDTO;
 import br.upe.booklubapi.app.user.dtos.CreateUserDTO;
+import br.upe.booklubapi.app.user.dtos.KeycloakUserDTO;
+import br.upe.booklubapi.app.user.dtos.UpdateUserDTO;
+import br.upe.booklubapi.app.user.services.UserMediaStorageService;
+import br.upe.booklubapi.domain.users.entities.KeycloakUser;
 import br.upe.booklubapi.infra.core.KeycloakClient;
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -13,10 +19,22 @@ import reactor.core.publisher.Mono;
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final KeycloakClient keycloakClient;
+    private final CreateUserDTOMapper createUserDTOMapper;
+    private final UserMediaStorageService userMediaStorageService;
 
     @Override
-    public Mono<Void> register(CreateUserDTO userDTO) {
-        return keycloakClient.register(userDTO);
+    public Mono<Void> register(CreateUserDTO userDTO) { //Essa gambiarra absurda tem que ser feita porque a imagem de perfil tem que ser salva usando o UUID
+        if (userDTO.image() == null) {
+            return keycloakClient.register(userDTO);
+        }
+        
+        keycloakClient.register(userDTO);
+        KeycloakUserDTO savedUser = keycloakClient.getUserByEmail(userDTO.email()).block().get(0); //Trocar isso aqui pra usar o JPA dps
+        UUID userUUID = UUID.fromString(savedUser.id());
+
+        String imagePath = userMediaStorageService.saveProfilePicture(userDTO.image(), userUUID);
+        
+        return keycloakClient.updateProfilePicturePathById(imagePath, userUUID);
     }
 
     @Override
@@ -24,3 +42,4 @@ public class AuthServiceImpl implements AuthService {
         return keycloakClient.login(authBody);
     }
 }
+,
--- src/main/java/br/upe/booklubapi/app/user/dtos/mappers/CreateUserDTOMapper.java
+++ src/main/java/br/upe/booklubapi/app/user/dtos/mappers/CreateUserDTOMapper.java
@@ -1,30 +1,37 @@
 package br.upe.booklubapi.app.user.dtos.mappers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.mapstruct.*;
+import org.springframework.stereotype.Component;
+
 import br.upe.booklubapi.app.user.dtos.CreateUserDTO;
+import br.upe.booklubapi.app.user.services.UserMediaStorageService;
+import br.upe.booklubapi.domain.users.entities.KeycloakUser;
 import br.upe.booklubapi.domain.users.entities.User;
+import lombok.AllArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Mapper(
     componentModel=MappingConstants.ComponentModel.SPRING,
-    unmappedTargetPolicy=ReportingPolicy.IGNORE
+    unmappedTargetPolicy=ReportingPolicy.IGNORE,
+    uses=CreateUserDTOMapperHelper.class
 )
+
 public interface CreateUserDTOMapper {
-
-    /**
-     * Gera um objeto {@link User} a partir de {@link CreateUserDTO}.
-     * <br>
-     * <br>
-     * <b>OBS.:</b> a propriedade imageUrl do usuário não é resolvida por este
-     * mapper. Portanto, você mesmo deve gerar a url da imagem para settar
-     * no objeto {@link User} gerado. Por exemplo:
-     * <pre>
-     *  User user = createUserDTOMapper.toEntity(createUserDTO);
-     *  user.setImageUrl(fooMediaStorage.saveImage(
-     *      createUserDTO.image()
-     *  ));
-     * </pre>
-     * @return {@link User} sem o atributo <code>imageUrl</code> definido
-     */
-    User toEntity(CreateUserDTO createUserDTO);
-
+    // @Mapping(source="image", source="username", target= "attributes", qualifiedByName ="imageUrltoAttributes")
+    KeycloakUser toKeycloakUser(CreateUserDTO createUserDTO);
 }
+
+// @Component
+// @AllArgsConstructor
+// class CreateUserDTOMapperHelper {
+//     UserMediaStorageService userMediaStorageService;
+
+//     @Named("imageUrltoAttributes")
+//     public Map<String, List<String>> handleImageUrlMapping(MultipartFile image, String username) {
+//         String imagePath = userMediaStorageService.saveProfilePicture(image, username);
+//     }
+
+// }
