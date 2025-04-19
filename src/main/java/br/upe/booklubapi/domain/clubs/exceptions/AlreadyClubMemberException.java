8a5319f967c143d3658b1d94f491eaaf5f4f7c63
Luan-Vn4 <luanvilaca1@gmail.com>
2025-04-19 16:07:21 -0300

    fix: corrigido conflito de construtores da exceções AlreadyClubMemberException e NotClubMemberException

--- src/main/java/br/upe/booklubapi/domain/clubs/exceptions/AlreadyClubMemberException.java
+++ src/main/java/br/upe/booklubapi/domain/clubs/exceptions/AlreadyClubMemberException.java
@@ -1,14 +1,11 @@
 package br.upe.booklubapi.domain.clubs.exceptions;
 
-import lombok.experimental.StandardException;
-
 import java.util.UUID;
 
-@StandardException
 public class AlreadyClubMemberException extends RuntimeException {
 
   public AlreadyClubMemberException(UUID userId, UUID clubId) {
-    this((
+    super((
         "User with id \"%s\" is already a member of club with id \"%s\"."
     ).formatted(userId, clubId));
   }
