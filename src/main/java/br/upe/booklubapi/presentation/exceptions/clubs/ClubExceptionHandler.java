6b3c94a62ffa4afe9f869ebd59d58c847eb1ee0f
Luan-Vn4 <luanvilaca1@gmail.com>
2025-04-19 00:48:57 -0300

    feat: criado exception handler para lidar com exceções no domínio de clubes

---
+++ src/main/java/br/upe/booklubapi/presentation/exceptions/clubs/ClubExceptionHandler.java
@@ -0,0 +1,76 @@
+package br.upe.booklubapi.presentation.exceptions.clubs;
+
+import br.upe.booklubapi.domain.clubs.exceptions.AlreadyClubMemberException;
+import br.upe.booklubapi.domain.clubs.exceptions.ClubNotFoundException;
+import br.upe.booklubapi.domain.clubs.exceptions.NotClubMemberException;
+import br.upe.booklubapi.domain.clubs.exceptions.UnauthorizedClubActionException;
+import br.upe.booklubapi.presentation.exceptions.core.ExceptionBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ClubExceptionHandler {
+
+    @ExceptionHandler(AlreadyClubMemberException.class)
+    public ResponseEntity<ExceptionBody> handle(AlreadyClubMemberException e) {
+        final HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        final var resp = ExceptionBody.builder()
+            .httpStatus(status.value())
+            .error("Already a member")
+            .message(e.getMessage())
+            .timestamp(Instant.now())
+            .build();
+
+        return ResponseEntity.status(status).body(resp);
+    }
+
+    @ExceptionHandler(ClubNotFoundException.class)
+    public ResponseEntity<ExceptionBody> handle(ClubNotFoundException e) {
+        final HttpStatus status = HttpStatus.NOT_FOUND;
+
+        final var resp = ExceptionBody.builder()
+            .httpStatus(status.value())
+            .error("Club not found")
+            .message(e.getMessage())
+            .timestamp(Instant.now())
+            .build();
+
+        return ResponseEntity.status(status).body(resp);
+    }
+
+    @ExceptionHandler(NotClubMemberException.class)
+    public ResponseEntity<ExceptionBody> handle(NotClubMemberException e) {
+        final HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        final var resp = ExceptionBody.builder()
+            .httpStatus(status.value())
+            .error("Not a member")
+            .message(e.getMessage())
+            .timestamp(Instant.now())
+            .build();
+
+        return ResponseEntity.status(status).body(resp);
+    }
+
+    @ExceptionHandler(UnauthorizedClubActionException.class)
+    public ResponseEntity<ExceptionBody> handle(
+        UnauthorizedClubActionException e
+    ) {
+        final HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        final var resp = ExceptionBody.builder()
+            .httpStatus(status.value())
+            .error("Unauthorized club")
+            .message(e.getMessage())
+            .timestamp(Instant.now())
+            .build();
+
+        return ResponseEntity.status(status).body(resp);
+    }
+
+}
--- src/main/java/br/upe/booklubapi/presentation/exceptions/core/ExceptionBody.java
+++ src/main/java/br/upe/booklubapi/presentation/exceptions/core/ExceptionBody.java
@@ -1,15 +1,12 @@
 package br.upe.booklubapi.presentation.exceptions.core;
 
 import java.time.Instant;
-
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 
-@AllArgsConstructor
 @Builder
-public class ExceptionBody {
-    private int httpStatus;
-    private String error;
-    private String message;
-    private Instant timestamp;
-}
+public record ExceptionBody(
+    int httpStatus,
+    String error,
+    String message,
+    Instant timestamp
+) {}
