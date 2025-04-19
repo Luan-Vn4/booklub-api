45c8870042d763c62aecfb71cc2ab3ecd07e67a0
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-04-19 09:28:41 -0300

    refactor: refatorando o codigo do keycloak client pra usar maps na hora de construir jsons

--- src/main/java/br/upe/booklubapi/infra/core/KeycloakClient.java
+++ src/main/java/br/upe/booklubapi/infra/core/KeycloakClient.java
@@ -1,6 +1,9 @@
 package br.upe.booklubapi.infra.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -8,6 +11,10 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.upe.booklubapi.utils.KeycloakUtils;
 import br.upe.booklubapi.app.auth.dto.AuthBody;
 import br.upe.booklubapi.app.auth.dto.KeycloakTokenDTO;
@@ -26,148 +33,178 @@ import java.util.UUID;
 @AllArgsConstructor
 public class KeycloakClient {
 
-    @Qualifier("keycloakWebClient")
-    private final WebClient keycloakWebClient;
+        @Qualifier("keycloakWebClient")
+        private final WebClient keycloakWebClient;
 
-    private final KeycloakUtils keycloakUtils;
+        private final KeycloakUtils keycloakUtils;
 
-    private final KeycloakProperties keycloakProperties;
+        private final KeycloakProperties keycloakProperties;
 
-    public Mono<UserDTO> getUserById(UUID uuid) {
-        String adminToken = keycloakUtils.getAdminToken();
+        private final ObjectMapper objectMapper;
 
-        return keycloakWebClient
-                .get()
-                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
-                        + "/users/" + uuid)
-                .header("Authorization", "Bearer " + adminToken)
-                .retrieve()
-                .bodyToMono(UserDTO.class);
-    }
+        public Mono<UserDTO> getUserById(UUID uuid) {
+                String adminToken = keycloakUtils.getAdminToken();
 
-    public Mono<List<UserDTO>> getUserByEmail(String email) {
-        String adminToken = keycloakUtils.getAdminToken();
+                return keycloakWebClient
+                                .get()
+                                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
+                                                + "/users/" + uuid)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .retrieve()
+                                .bodyToMono(UserDTO.class);
+        }
 
-        return keycloakWebClient
-                .get()
-                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
-                        + "/users?email=" + email)
-                .header("Authorization", "Bearer " + adminToken)
-                .retrieve()
-                .bodyToFlux(UserDTO.class)
-                .collectList();
-    }
+        public Mono<List<UserDTO>> getUserByEmail(String email) {
+                String adminToken = keycloakUtils.getAdminToken();
 
-    public Mono<Void> deleteUserById(UUID uuid) {
-        String adminToken = keycloakUtils.getAdminToken();
+                return keycloakWebClient
+                                .get()
+                                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
+                                                + "/users?email=" + email)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .retrieve()
+                                .bodyToFlux(UserDTO.class)
+                                .collectList();
+        }
 
-        return keycloakWebClient
-                .delete()
-                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
-                        + "/users/" + uuid)
-                .header("Authorization", "Bearer " + adminToken)
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
+        public Mono<Void> deleteUserById(UUID uuid) {
+                String adminToken = keycloakUtils.getAdminToken();
 
-    public Mono<Void> updateUserById(User updatedUser, UUID uuid) {
-        String adminToken = keycloakUtils.getAdminToken();
+                return keycloakWebClient
+                                .delete()
+                                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
+                                                + "/users/" + uuid)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .retrieve()
+                                .bodyToMono(Void.class);
+        }
 
-        String user = "{"
-                + "\"username\": \"" + updatedUser.getUsername() + "\","
-                + "\"email\": \"" + updatedUser.getEmail() + "\","
-                + "\"firstName\": \"" + updatedUser.getFirstName() + "\","
-                + "\"lastName\": \"" + updatedUser.getLastName() + "\","
-                + "\"enabled\": true,"
-                + "\"attributes\": {"
-                + "\"imageUrl\": [\"" + updatedUser.getAttributes().get("imageUrl") + "\"]"
-                + "}"
-                + "}";
+        public Mono<Void> updateUserById(User updatedUser, UUID uuid) {
+                String adminToken = keycloakUtils.getAdminToken();
 
-        return keycloakWebClient
-                .put()
-                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
-                        + "/users/" + uuid)
-                .header("Authorization", "Bearer " + adminToken)
-                .header("Content-Type", "application/json")
-                .bodyValue(user)
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
+                // Create the user map
+                Map<String, Object> userMap = new HashMap<>();
+                userMap.put("username", updatedUser.getUsername());
+                userMap.put("email", updatedUser.getEmail());
+                userMap.put("firstName", updatedUser.getFirstName());
+                userMap.put("lastName", updatedUser.getLastName());
+                userMap.put("enabled", true);
 
-    public Mono<Void> updateProfilePicturePathById(String newProfilePicturePath, UserDTO saveduser, UUID id) {
-        String adminToken = keycloakUtils.getAdminToken();
+                // Set attributes
+                Map<String, Object> attributes = new HashMap<>();
+                attributes.put("imageUrl", List.of(updatedUser.getAttributes().get("imageUrl"))); // Wrap imageUrl in a
+                                                                                                  // list
+                userMap.put("attributes", attributes);
 
-        String user = "{"
-                + "\"username\": \"" + saveduser.username() + "\","
-                + "\"email\": \"" + saveduser.email() + "\","
-                + "\"firstName\": \"" + saveduser.firstName() + "\","
-                + "\"lastName\": \"" + saveduser.lastName() + "\","
-                + "\"enabled\": true,"
-                + "\"attributes\": {"
-                + "\"imageUrl\": [\"" + newProfilePicturePath + "\"]"
-                + "}"
-                + "}";
-        
-        return keycloakWebClient
-                .put()
-                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
-                        + "/users/" + id)
-                .header("Authorization", "Bearer " + adminToken)
-                .header("Content-Type", "application/json")
-                .bodyValue(user)
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
+                String userJson = "";
+                try {
+                        userJson = objectMapper.writeValueAsString(userMap);
+                } catch (JsonProcessingException e) {
+                        throw new RuntimeException("Error converting user map to JSON", e);
+                }
 
-    public Mono<Void> register(CreateUserDTO userDTO) {
-        String adminToken = keycloakUtils.getAdminToken();
+                return keycloakWebClient
+                                .put()
+                                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
+                                                + "/users/" + uuid)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .header("Content-Type", "application/json")
+                                .bodyValue(userJson)
+                                .retrieve()
+                                .bodyToMono(Void.class);
+        }
 
-        String user = "{"
-                + "\"username\": \"" + userDTO.username() + "\","
-                + "\"email\": \"" + userDTO.email() + "\","
-                + "\"firstName\": \"" + userDTO.firstName() + "\","
-                + "\"lastName\": \"" + userDTO.lastName() + "\","
-                + "\"enabled\": true,"
-                + "\"credentials\": [{"
-                + "\"type\": \"password\","
-                + "\"value\": \"" + userDTO.password() + "\","
-                + "\"temporary\": false"
-                + "}],"
-                + "\"attributes\": {"
-                + "\"imageUrl\": \"" + null + "\""
-                + "}"
-                + "}";
+        public Mono<Void> updateProfilePicturePathById(String newProfilePicturePath, UserDTO userBeingUpdated, UUID id) {
+                String adminToken = keycloakUtils.getAdminToken();
+                
 
-        return keycloakWebClient
-                .post()
-                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
-                        + "/users")
-                .header("Authorization", "Bearer " + adminToken)
-                .header("Content-Type", "application/json")
-                .bodyValue(user)
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
+                Map<String, Object> userMap = new HashMap<>();
+                userMap.put("username", userBeingUpdated.username());
+                userMap.put("email", userBeingUpdated.email());
+                userMap.put("firstName", userBeingUpdated.firstName());
+                userMap.put("lastName", userBeingUpdated.lastName());
 
-    public Mono<KeycloakTokenDTO> login(AuthBody authBody) {
+                Map<String, Object> attributes = new HashMap<>();
+                attributes.put("imageUrl", List.of(newProfilePicturePath));
+                userMap.put("attributes", attributes);
 
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("client_id", keycloakProperties.getClientId());
-        formData.add("client_secret", keycloakProperties.getClientSecret());
-        formData.add("username", authBody.username());
-        formData.add("password", authBody.password());
-        formData.add("grant_type", "password");
+                String userJson = "";
+                try {
+                        userJson = objectMapper.writeValueAsString(userMap);
+                } catch (JsonProcessingException e) {
+                        throw new RuntimeException("Error converting user map to JSON", e);
+                }
 
-        String tokenUrl = "/realms/" + keycloakProperties.getClientRealm()
-                + "/protocol/openid-connect/token";
+                return keycloakWebClient
+                                .put()
+                                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
+                                                + "/users/" + id)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .header("Content-Type", "application/json")
+                                .bodyValue(userJson)
+                                .retrieve()
+                                .bodyToMono(Void.class);
+        }
 
-        return keycloakWebClient.post()
-                .uri(tokenUrl)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters.fromFormData(formData))
-                .retrieve()
-                .bodyToMono(KeycloakTokenDTO.class);
-    }
+        public Mono<Void> register(CreateUserDTO userDTO) {
+                String adminToken = keycloakUtils.getAdminToken();
+
+                Map<String, Object> userMap = new HashMap<>();
+
+                userMap.put("username", userDTO.username());
+                userMap.put("email", userDTO.email());
+                userMap.put("firstName", userDTO.firstName());
+                userMap.put("lastName", userDTO.lastName());
+                userMap.put("enabled", true);
+
+                List<Map<String, Object>> credentials = new ArrayList<>();
+                
+                Map<String, Object> credentialsMap = new HashMap<>();
+
+                credentialsMap.put("type","password");
+                credentialsMap.put("temporary","false");
+                credentialsMap.put("value", userDTO.password());
+
+                credentials.add(credentialsMap);
+
+                userMap.put("credentials", credentials);
+
+                String userJson = "";
+                try {
+                        userJson = objectMapper.writeValueAsString(userMap);
+                } catch (JsonProcessingException e) {
+                        throw new RuntimeException("Error converting user map to JSON", e);
+                }
+
+                return keycloakWebClient
+                                .post()
+                                .uri("/admin/realms/" + keycloakProperties.getClientRealm()
+                                                + "/users")
+                                .header("Authorization", "Bearer " + adminToken)
+                                .header("Content-Type", "application/json")
+                                .bodyValue(userJson)
+                                .retrieve()
+                                .bodyToMono(Void.class);
+        }
+
+        public Mono<KeycloakTokenDTO> login(AuthBody authBody) {
+
+                MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+                formData.add("client_id", keycloakProperties.getClientId());
+                formData.add("client_secret", keycloakProperties.getClientSecret());
+                formData.add("username", authBody.username());
+                formData.add("password", authBody.password());
+                formData.add("grant_type", "password");
+
+                String tokenUrl = "/realms/" + keycloakProperties.getClientRealm()
+                                + "/protocol/openid-connect/token";
+
+                return keycloakWebClient.post()
+                                .uri(tokenUrl)
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .body(BodyInserters.fromFormData(formData))
+                                .retrieve()
+                                .bodyToMono(KeycloakTokenDTO.class);
+        }
 
 }
