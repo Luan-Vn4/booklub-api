635693b383def27431ebbcf7db31e153f7125602
Luan-Vn4 <luanvilaca1@gmail.com>
2025-04-20 11:27:12 -0300

    fix: adicionado cascading às relações de entrada pendentes
    Agora, ao apagar usuários ou clubes, os pedidos de entrada pendentes ligados a eles também são deletados

--- src/main/java/br/upe/booklubapi/domain/clubs/entities/ClubPendingEntry.java
+++ src/main/java/br/upe/booklubapi/domain/clubs/entities/ClubPendingEntry.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.JdbcType;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 @Entity
@@ -23,11 +25,13 @@ public class ClubPendingEntry {
     @ManyToOne
     @MapsId("clubId")
     @JoinColumn(name="club_id")
+    @OnDelete(action=OnDeleteAction.CASCADE)
     private Club club;
 
     @ManyToOne
     @MapsId("userId")
     @JoinColumn(name="user_id")
+    @OnDelete(action=OnDeleteAction.CASCADE)
     private User user;
 
     @Enumerated(EnumType.STRING)
