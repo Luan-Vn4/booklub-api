e48dca10de2a4307342ef538ff0c4d34d3256593
Luan-Vn4 <luanvilaca1@gmail.com>
2025-04-20 15:24:06 -0300

    fix: corrigido import inexistente em UserServiceImpl

--- src/main/java/br/upe/booklubapi/app/user/services/UserServiceImpl.java
+++ src/main/java/br/upe/booklubapi/app/user/services/UserServiceImpl.java
@@ -1,14 +1,7 @@
 package br.upe.booklubapi.app.user.services;
 
-import java.util.List;
 import java.util.UUID;
-
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.multipart.MultipartFile;
-
 import br.upe.booklubapi.app.user.dtos.UpdateUserDTO;
 import br.upe.booklubapi.app.user.dtos.UserDTO;
 import br.upe.booklubapi.app.user.dtos.mappers.UpdateUserDTOMapper;
@@ -17,9 +10,7 @@ import reactor.core.publisher.Mono;
 import br.upe.booklubapi.domain.users.entities.User;
 import br.upe.booklubapi.domain.users.repository.UserRepository;
 import br.upe.booklubapi.infra.core.gateways.Keycloak.KeycloakRestApiGateway;
-import br.upe.booklubapi.presentation.exceptions.UserHasNoPermissionToException;
 import br.upe.booklubapi.utils.UserUtils;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
