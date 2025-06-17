902e9237a5027a10e56a0e68411dc503ff239301
holyvieri <beatriz.oliveirac@upe.br>
2025-06-17 10:49:22 -0300

    feat(books): id e date nos livros

--- src/main/java/br/upe/booklubapi/app/books/dtos/BookItem.java
+++ src/main/java/br/upe/booklubapi/app/books/dtos/BookItem.java
@@ -4,15 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.bouncycastle.jcajce.provider.symmetric.SM4;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class BookItem {
+    private String id;
     private String title;
     private String authors;
     private String description;
     private String thumbnail;
     private String isbn;
+    private String publishedDate;
 }
